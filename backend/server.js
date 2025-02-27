e3e9ea107bea7a895f48d0fb53b91a8b63ac059f
DeepaKumari <DeepaKumari@fico.com>
2025-02-27 22:31:47 +0530

    Changes related to removal of get from server.js file

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-    res.send('API is running......');
-});
+// app.get('/', (req,res) => {
+//     res.send('API is running......');
+// });
 
 app.get('/api/config/paypal', (req, res) =>
     res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
