5861fd6ed46f53a7848c8c5f7411e75940aff3ff
DeepaKumari <DeepaKumari@fico.com>
2025-02-27 20:49:52 +0530

    Changes related to build and deploy commands

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,22 @@ app.use(errorHandler);
 //     res.json(product);
 // })
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+
 app.listen(port, () => {
     console.log(`Server running on port${port}`);
 })
